cf640883a6f2bbbeeb5fca2b6578eb2eee4a511f
zakaria bellachia <119078569+zakideep@users.noreply.github.com>
2025-09-13 14:03:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from telegram import Bot, Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-
+from diffusers import DiffusionPipeline
+import torch
 # load .env
 load_dotenv("token.env")
 
 # get tokens
 bot_token = os.getenv("BOT_TOKEN")
-hf_token = os.getenv("HF_TOKEN")
 
 # banned words to generate
 with open("banned_words.json", "r") as f:
@@ -24,17 +24,12 @@ def check_prompt(prompt):
             return False, word
     return True, None
 
-# Hugging Face API call
-def generate_image(prompt: str, model_id="stabilityai/stable-diffusion-xl-base-1.0"):
-    api_url = f"https://api-inference.huggingface.co/models/{model_id}"
-    headers = {"Authorization": f"Bearer {hf_token}"}
-    payload = {"inputs": prompt}
+# diffusion pipeline
+model_id="xyn-ai/openjourney"
+pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+pipe = pipe.to("cpu")
 
-    response = requests.post(api_url, headers=headers, json=payload)
-    if response.status_code == 200:
-        return response.content  # image bytes
-    else:
-        raise Exception(f"HF API error {response.status_code}: {response.text}")
+
 
 # /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -46,16 +41,11 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     is_valid, bad_word = check_prompt(user_input)
     if is_valid:
-        try:
-            img_bytes = generate_image(user_input)
-            filename = f"{user_input}.png"
-            with open(filename, "wb") as f:
-                f.write(img_bytes)
-
-            with open(filename, "rb") as img_file:
-                await update.message.reply_document(img_file)
-        except Exception as e:
-            await update.message.reply_text(f"⚠️ Error: {e}")
+        image = pipe(user_input).images[0]
+        filename = f"{user_input}.png"
+        image.save(filename)
+        with open(filename, "rb") as img_file:
+            await update.message.reply_document(img_file)
     else:
         await update.message.reply_text(f"🚫 The word '{bad_word}' is not allowed to generate")
 
@@ -72,6 +62,7 @@ def main():
 if __name__ == "__main__":
     keep_alive()
     main()
+
 
 
 
